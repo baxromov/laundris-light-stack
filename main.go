ebff1abffa2939cec5a693bae969e1fd7580a84c
SHOKHZODBEK <baxromov.shahzodbek@gmail.com>
2025-05-14 16:26:25 +0500

    Increase WebSocket keep-alive interval and clean up comments
    Updated the keepAliveInterval to 10 seconds to reduce ping frequency. Removed redundant and verbose comments to improve code readability. No functional behavior was altered except for the keep-alive timing adjustment.

--- main.go
+++ main.go
@@ -18,15 +18,14 @@ type Command struct {
 
 const (
 	wsURL               = "wss://laundirs-supply-chain-websocket.azurewebsites.net/light-stack"
-	keepAliveInterval   = 3 * time.Second  // Interval to send ping messages
-	connectionReadLimit = 60 * time.Second // Maximum time to wait for server response
+	keepAliveInterval   = 10 * time.Second
+	connectionReadLimit = 60 * time.Second
 )
 
 func main() {
 	for {
 		log.Println("Attempting to connect to WebSocket server...")
 
-		// Establish WebSocket connection
 		conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 		if err != nil {
 			log.Printf("Failed to connect to WebSocket: %v. Retrying in 2 seconds...", err)
@@ -36,30 +35,25 @@ func main() {
 
 		log.Println("Connected to WebSocket server")
 
-		// Start a goroutine to send keep-alive ping messages
 		done := make(chan struct{})
 		go keepAlive(conn, done)
 
-		// Handle WebSocket messages
 		err = handleMessages(conn, done)
 		if err != nil {
 			log.Printf("Connection lost: %v", err)
 		}
 
 		log.Println("Disconnected. Reconnecting...")
-		time.Sleep(2 * time.Second) // Wait before attempting to reconnect
+		time.Sleep(2 * time.Second)
 	}
 }
 
-// handleMessages handles incoming WebSocket messages and processes commands
 func handleMessages(conn *websocket.Conn, done chan struct{}) error {
-	defer close(done) // Signal the keepAlive goroutine to exit
+	defer close(done)
 	defer conn.Close()
 
-	// Set a read limit duration for the connection
 	conn.SetReadDeadline(time.Now().Add(connectionReadLimit))
 	conn.SetPongHandler(func(appData string) error {
-		// Update the read deadline when a Pong is received
 		conn.SetReadDeadline(time.Now().Add(connectionReadLimit))
 		return nil
 	})
@@ -67,7 +61,6 @@ func handleMessages(conn *websocket.Conn, done chan struct{}) error {
 	for {
 		var cmd Command
 
-		// Read message from WebSocket
 		err := conn.ReadJSON(&cmd)
 		if err != nil {
 			return fmt.Errorf("error reading message: %w", err)
@@ -75,7 +68,6 @@ func handleMessages(conn *websocket.Conn, done chan struct{}) error {
 
 		log.Printf("Received command: %+v", cmd)
 
-		// Send HTTP POST request based on the command
 		err = sendHTTPRequest(cmd)
 		if err != nil {
 			log.Printf("Failed to process command: %v", err)
@@ -83,7 +75,6 @@ func handleMessages(conn *websocket.Conn, done chan struct{}) error {
 	}
 }
 
-// keepAlive sends periodic ping messages to keep the WebSocket connection alive
 func keepAlive(conn *websocket.Conn, done chan struct{}) {
 	ticker := time.NewTicker(keepAliveInterval)
 	defer ticker.Stop()
@@ -91,10 +82,8 @@ func keepAlive(conn *websocket.Conn, done chan struct{}) {
 	for {
 		select {
 		case <-done:
-			// Exit the keepAlive routine when the connection is closed
 			return
 		case <-ticker.C:
-			// Send a ping message
 			err := conn.WriteMessage(websocket.PingMessage, nil)
 			if err != nil {
 				log.Printf("Failed to send ping: %v", err)
@@ -105,23 +94,18 @@ func keepAlive(conn *websocket.Conn, done chan struct{}) {
 	}
 }
 
-// sendHTTPRequest sends an HTTP POST request to the given API endpoint
 func sendHTTPRequest(cmd Command) error {
-	// Construct the request URL
 	apiURL := fmt.Sprintf("http://localhost:8080/api/device/gpo/light/%s?mode=%s&turnOn=%t", cmd.DeviceID, cmd.Mode, cmd.TurnOn)
 
 	log.Printf("Sending HTTP POST to %s", apiURL)
 
-	// Create an HTTP POST request
 	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer([]byte{}))
 	if err != nil {
 		return fmt.Errorf("failed to create HTTP request: %w", err)
 	}
 
-	// Set appropriate headers
 	req.Header.Set("Content-Type", "application/json")
 
-	// Send the request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -129,7 +113,6 @@ func sendHTTPRequest(cmd Command) error {
 	}
 	defer resp.Body.Close()
 
-	// Check response status
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected response status: %d", resp.StatusCode)
 	}
